19695ef8ebc75f15c37a4670a8f08f5d55d1100d
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-21 18:31:20 +0430

    Update files structure according ti gitHub actions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <h1>Hello world</h1>
+  </React.StrictMode>,
 );
