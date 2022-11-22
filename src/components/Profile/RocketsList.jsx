2fa044ad0763fb8b74cde506ba01fc5136ae27f9
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-22 22:53:36 +0430

    Add table style

--- src/components/Profile/RocketsList.jsx
+++ src/components/Profile/RocketsList.jsx
@@ -2,6 +2,9 @@ export default function RocketsList() {
   return (
     <div className="rocket-list">
       <h1>My Rockets</h1>
+      <ul className="rockets-nav">
+        <li className="nav-item">Hi</li>
+      </ul>
     </div>
   );
 }
