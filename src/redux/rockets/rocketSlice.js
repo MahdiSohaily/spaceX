b5830a403e40f39f0dd8396d5e0ffd683f74e652
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-22 20:19:46 +0430

    Add data normalizer file

--- src/redux/rockets/rocketSlice.js
+++ src/redux/rockets/rocketSlice.js
@@ -1,6 +1,11 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { client } from '../../api/client';
 
+export const fetchRockets = createAsyncThunk('getRockets', async () => {
+  const result = await client.get('https://api.spacexdata.com/v3/rockets');
+  console.log(result);
+});
+
 const initialState = {
   entities: {},
   status: 'ideal',
@@ -10,11 +15,11 @@ const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
   reducers: {},
-});
-
-export const fetchRockets = createAsyncThunk('getRockets', async () => {
-  const result = await client.get('https://api.spacexdata.com/v3/rockets');
-  console.log(result);
+  extraReducers: (builder) => {
+    builder.addCase(fetchRockets.fulfilled, () => {
+      console.log('done');
+    });
+  },
 });
 
 export default rocketsSlice.reducer;
