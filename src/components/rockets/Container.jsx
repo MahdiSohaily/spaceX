aa6ca1edc3e81781e91b45d7848f139a5d830646
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-22 21:38:27 +0430

    Add loader to the UI

--- src/components/rockets/Container.jsx
+++ src/components/rockets/Container.jsx
@@ -1,4 +1,5 @@
 import { shallowEqual, useSelector } from 'react-redux';
+import { RotatingLines } from 'react-loader-spinner';
 import rocketsData from '../../redux/rockets/selectors';
 import Card from './Card';
 
@@ -6,6 +7,18 @@ export default function Container() {
   const status = useSelector((state) => state.rockets.status);
   const rockets = useSelector(rocketsData, shallowEqual);
   const cards = rockets.map((item) => <Card key={item} rocketId={item} />);
-  const result = status === 'loading' ? <p>Loading</p> : cards;
+  const result = status === 'loading' ? (
+    <div className="loader">
+      <RotatingLines
+        strokeColor="dodgerblue"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="96"
+        visible
+      />
+    </div>
+  ) : (
+    cards
+  );
   return <div>{result}</div>;
 }
