2601a344fc183c41b57ab3b9ad97e9880cda000b
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-22 12:33:29 +0430

    Add project different parts slice

---
+++ src/redux/missions/missionsSlice.js
@@ -0,0 +1,11 @@
+const { createSlice } = require('@reduxjs/toolkit');
+
+const initialState = {};
+
+const missionsSlice = createSlice({
+  name: 'missions',
+  initialState,
+  reducers: {},
+});
+
+export default missionsSlice.reducer;
---
+++ src/redux/rockets/rocketSlice.js
@@ -0,0 +1,11 @@
+const { createSlice } = require('@reduxjs/toolkit');
+
+const initialState = {};
+
+const rocketsSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  reducers: {},
+});
+
+export default rocketsSlice.reducer;
