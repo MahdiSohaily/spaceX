85bbb589d4116287bbb58850dca3054c300c0aae
luccamicale <luccamicale2015@gmail.com>
2022-11-22 19:20:17 -0300

    add the fetch and connect with API

--- src/pages/Missions.jsx
+++ src/pages/Missions.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 
-export default function Missions() {
+
+const Mission = ({
+  id,
+  missionName,
+  description,
+}) => {
+  const dispatch = useDispatch();
+  const reserved = () => (
+    dispatch(reservedMissions(id))
+  );
   return(
     <div className="wrapper">
       <table className='table'>
         <thead>
           <tr>
-            <th className='mission'>Mission</th>
-            <th className='description'>Description</th>
+            <th className='mission'>{missionName}</th>
+            <th className='description'>{description}</th>
             <th className='status'>Status</th>
             <th className='join'>.</th>
           </tr>
         </thead>
       </table>
     </div>
-  )
-}
+  );
+};
+
+export default Mission;
+Mission.propTypes = {
+  id: PropTypes.string.isRequired,
+  missionName: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -1,11 +1,37 @@
-const { createSlice } = require('@reduxjs/toolkit');
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = {};
-
-const missionsSlice = createSlice({
+const FETCH_MISSIONS = 'space-travelers/src/redux/missions/fetchMissions';
+const url = 'https://api.spacexdata.com/v3/missions/';
+export const fetchMissions = createAsyncThunk(FETCH_MISSIONS, () => axios.get(url)
+  .then((response) => {
+    const missions = response.data;
+    const data = Object.keys(missions).map((id) => ({
+      id: missions[id].mission_id,
+      missionName: missions[id].mission_name,
+      missionDescription: missions[id].description,
+      reserved: false,
+    }));
+    return data;
+  }));
+const initialState = [];
+const missionSlice = createSlice({
   name: 'missions',
   initialState,
-  reducers: {},
+  reducers: {
+    reservedMissions(state, action) {
+      const newState = state.map((index) => {
+        if (action.payload !== index.id) {
+          return { ...index };
+        }
+        return { ...index, reserved: true };
+      });
+      return newState;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchMissions.fulfilled, (_, action) => action.payload);
+  },
 });
-
-export default missionsSlice.reducer;
+export const { reservedMissions } = missionSlice.actions;
+export default missionSlice.reducer;
