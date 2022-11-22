a23ca0d6d21f9da55cfa133e2ed731b21e7b2e91
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-22 20:52:18 +0430

    Add dynamic cards

--- src/components/rockets/Container.jsx
+++ src/components/rockets/Container.jsx
@@ -1,16 +1,9 @@
-import { useSelector } from 'react-redux';
+import { shallowEqual, useSelector } from 'react-redux';
 import rocketsData from '../../redux/rockets/selectors';
 import Card from './Card';
 
 export default function Container() {
-  const rockets = useSelector(rocketsData);
-  console.log(rockets);
-  return (
-    <div>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
-  );
+  const rockets = useSelector(rocketsData, shallowEqual);
+  const cards = rockets.map((item) => <Card key={item} id={item} />);
+  return <div>{cards}</div>;
 }
