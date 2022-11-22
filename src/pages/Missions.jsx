84acfa4bfd2cc125939ac52fa76d2b0d8c5ce6cb
luccamicale <luccamicale2015@gmail.com>
2022-11-22 18:43:26 -0300

    add the structure

--- src/pages/Missions.jsx
+++ src/pages/Missions.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Missions() {
-  return <div className="wrapper">Missions</div>;
+  return(
+    <div className="wrapper">
+      <table className='table'>
+        <thead>
+          <tr>
+            <th className='mission'>Mission</th>
+            <th className='description'>Description</th>
+            <th className='status'>Status</th>
+            <th className='join'>.</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  )
 }
