7d380a3d1f758cf2fccd78a621673248d5aa8875
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-22 22:19:34 +0430

    Add reserved badge to the description

--- src/components/rockets/CardBody.jsx
+++ src/components/rockets/CardBody.jsx
@@ -1,14 +1,16 @@
+/* eslint-disable object-curly-newline */
 import PropTypes from 'prop-types';
 import ActionButton from './ActionButton';
 
-export default function CardBody({
-  id, name, description, active,
-}) {
+export default function CardBody({ id, name, description, active }) {
   return (
     <div className="card-body">
       <h1>{name}</h1>
       {active}
-      <p className="rocket-description">{description}</p>
+      <p className="rocket-description">
+        {active && <span className="badge text-neutral-100">Reserved</span>}
+        {description}
+      </p>
       <ActionButton active={active} id={id} />
     </div>
   );
