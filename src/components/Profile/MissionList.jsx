e1a0f59fc07b108209e11501f1f02a2175ac2fac
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-22 23:15:40 +0430

    Update empty list logic

--- src/components/Profile/MissionList.jsx
+++ src/components/Profile/MissionList.jsx
@@ -3,11 +3,7 @@ export default function MissionList() {
     <div className="rocket-list">
       <h1>My Rockets</h1>
       <ul className="rockets-nav">
-        <li className="nav-item">Hi</li>
-        <li className="nav-item">Hi</li>
-        <li className="nav-item">Hi</li>
-        <li className="nav-item">Hi</li>
-        <li className="nav-item">Hi</li>
+        <li className="nav-item"> </li>
       </ul>
     </div>
   );
--- src/components/Profile/RocketsList.jsx
+++ src/components/Profile/RocketsList.jsx
@@ -3,7 +3,15 @@ import { reservedRocket } from '../../redux/rockets/selectors';
 
 export default function RocketsList() {
   const reserved = useSelector(reservedRocket);
-  const items = reserved.map((item) => <li key={item.id}>{item.name}</li>);
+  const items = reserved.length > 0 ? (
+    reserved.map((item) => (
+      <li className="nav-item" key={item.id}>
+        {item.name}
+      </li>
+    ))
+  ) : (
+    <li className="nav-item">Nothing To Show</li>
+  );
   return (
     <div className="rocket-list">
       <h1>My Rockets</h1>
